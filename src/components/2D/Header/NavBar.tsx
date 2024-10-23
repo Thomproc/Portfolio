192291984122ce73633f368bdf45454c057ed935
Thomas <thomproc@hotmail.fr>
2024-10-24 01:30:41 +0200

    nettoyage nav bar

--- src/components/2D/Header/NavBar.tsx
+++ src/components/2D/Header/NavBar.tsx
@@ -8,97 +8,71 @@ export default function NavBar() {
   const location = useLocation();
 
   return (
-    <div className={styles.center}>
-      <div className={styles.navContainer}>
+    <div className={styles.container}>
+      <div
+        className={styles.navItem}
+        onClick={(_event) => {
+          if (location.pathname === EMainsRoutes.MAIN2D) {
+            navigate(EMainsRoutes.MAIN3D);
+            return;
+          }
+          navigate(EMainsRoutes.MAIN2D);
+        }}
+      >
+        {location.pathname === EMainsRoutes.MAIN2D
+          ? "Mon univers..."
+          : "Mon environnement..."}
+      </div>
+      <div className={styles.navBar}>
         <div
-          className={styles.navItem}
           onClick={(_event) => {
-            if (location.pathname === EMainsRoutes.MAIN2D) {
-              navigate(EMainsRoutes.MAIN3D);
-              return;
-            }
-            navigate(EMainsRoutes.MAIN2D);
+            Routes2DRefs[ERoutes2D.HOME].current?.scrollIntoView({
+              behavior: "smooth",
+              block: "center",
+            });
           }}
         >
-          {location.pathname === EMainsRoutes.MAIN2D
-            ? "Mon univers..."
-            : "Mon environnement..."}
+          Accueil
         </div>
-        <div className={styles.navBar}>
-          <div
-            // className={
-            // Mise en valeur de l'item en orange selon le pourcentage de scrollage
-            // location.pathname === ERoutes2D.HOME
-            //   ? "text-accent"
-            //   : styles.navItem
-            // }
-            onClick={(_event) => {
-              Routes2DRefs[ERoutes2D.HOME].current?.scrollIntoView({
-                behavior: "smooth",
-                block: "center",
-              });
-            }}
-          >
-            Accueil
-          </div>
-          <div
-            // className={
-            // Mise en valeur de l'item en orange selon le pourcentage de scrollage
-            // location.pathname === ERoutes2D.PROJECTS
-            //   ? "text-accent"
-            //   : styles.navItem
-            // }
-            onClick={(_event) => {
-              Routes2DRefs[ERoutes2D.PROJECTS].current?.scrollIntoView({
-                behavior: "smooth",
-                block: "center",
-              });
-            }}
-          >
-            Projets
-          </div>
-          <div
-            // className={
-            // Mise en valeur de l'item en orange selon le pourcentage de scrollage
-            // location.pathname === ERoutes2D.SKILLS
-            //   ? "text-accent"
-            //   : styles.navItem
-            // }
-            onClick={(_event) => {
-              Routes2DRefs[ERoutes2D.SKILLS].current?.scrollIntoView({
-                behavior: "smooth",
-                block: "center",
-              });
-            }}
-          >
-            Compétences
-          </div>
-          <div
-            // className={
-            // Mise en valeur de l'item en orange selon le pourcentage de scrollage
-            // location.pathname === ERoutes2D.CONTACT
-            //   ? "text-accent"
-            //   : styles.navItem
-            // }
-            onClick={(_event) => {
-              Routes2DRefs[ERoutes2D.CONTACT].current?.scrollIntoView({
-                behavior: "smooth",
-                block: "end",
-              });
-            }}
-          >
-            Contact
-          </div>
-        </div>
-        <a
-          href={"./CV.pdf"}
-          download={"CV_Procureur_Thomas"}
-          className={styles.navItem}
+        <div
+          onClick={(_event) => {
+            Routes2DRefs[ERoutes2D.PROJECTS].current?.scrollIntoView({
+              behavior: "smooth",
+              block: "center",
+            });
+          }}
         >
-          CV
-          <FileDownloadOutlinedIcon />
-        </a>
+          Projets
+        </div>
+        <div
+          onClick={(_event) => {
+            Routes2DRefs[ERoutes2D.SKILLS].current?.scrollIntoView({
+              behavior: "smooth",
+              block: "center",
+            });
+          }}
+        >
+          Compétences
+        </div>
+        <div
+          onClick={(_event) => {
+            Routes2DRefs[ERoutes2D.CONTACT].current?.scrollIntoView({
+              behavior: "smooth",
+              block: "end",
+            });
+          }}
+        >
+          Contact
+        </div>
       </div>
+      <a
+        href={"./CV.pdf"}
+        download={"CV_Procureur_Thomas"}
+        className={styles.navItem}
+      >
+        CV
+        <FileDownloadOutlinedIcon />
+      </a>
     </div>
   );
 }
